308f09a58c03e7b9f65cbd93f5150aadc8634fbd
Razvan <manea.razvan.valentin@gmail.com>
2023-01-28 19:04:13 +0200

    Display response to user after posting form data

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 	"github.com/alexedwards/scs/v2"
 	"github.com/ranecsutu/bookings/internal/config"
 	"github.com/ranecsutu/bookings/internal/handlers"
+	"github.com/ranecsutu/bookings/internal/models"
 	"github.com/ranecsutu/bookings/internal/render"
 )
 
@@ -19,6 +21,9 @@ var session *scs.SessionManager
 
 // main is the main function
 func main() {
+	//what am I going to put in the session
+	gob.Register(models.Reservation{})
+
 	// change this to true when in production
 	app.InProduction = false
 

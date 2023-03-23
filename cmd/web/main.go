1d861171ac5e63b284306e5cbd6ea37f8effc952
Razvan <manea.razvan.valentin@gmail.com>
2023-03-23 11:22:13 +0200

    Implement send mail functionality

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -32,6 +32,9 @@ func main() {
 	}
 	defer db.SQL.Close()
 
+	defer close(app.MailChan)
+	listenForMail()
+
 	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
 
 	srv := &http.Server{
@@ -51,6 +54,9 @@ func run() (*driver.DB, error) {
 	gob.Register(models.User{})
 	gob.Register(models.Room{})
 	gob.Register(models.Restriction{})
+
+	mailChan := make(chan models.MailData)
+	app.MailChan = mailChan
 
 	// change this to true when in production
 	app.InProduction = false
